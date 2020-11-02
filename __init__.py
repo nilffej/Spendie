b2835467409ede8607d1a9a7dd00f2cf23d68035
Jeff Lin <jeff.andrew.lin@gmail.com>
2020-11-02 17:13:50 -0500

    Fixed data mixing on simultaneous logins (hopefully)

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ playbackdata = {}
 def get_user_data():
     user = spotify.current_user()
     if not len(user.images):
-        pfp = "https://vignette.wikia.nocookie.net/caramella-girls/images/9/99/Blankpfp.png/revision/latest?cb=20190122015011"
+        pfp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     else:
         pfp = user.images[0].url
     data = {
@@ -62,16 +62,18 @@ def get_playback_data(token):
 
 # GETTING TOP TRACKS DATA
 
-def get_top_tracks(term):
-    data = []
-    toptracks = spotify.current_user_top_tracks(time_range=term,limit=50)
-    for item in toptracks.items:
-        data.append(get_track_data(item))
-    return data
+def get_top_tracks(term, token):
+    tempspotify = tk.Spotify()
+    with tempspotify.token_as(token):
+        data = []
+        toptracks = tempspotify.current_user_top_tracks(time_range=term,limit=50)
+        for item in toptracks.items:
+            data.append(get_track_data(item))
+        return data
 
 def get_track_data(track):
     if not len(track.album.images):
-        pfp = "https://vignette.wikia.nocookie.net/caramella-girls/images/9/99/Blankpfp.png/revision/latest?cb=20190122015011"
+        pfp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     else:
         pfp = track.album.images[0].url
     data = {
@@ -93,16 +95,18 @@ def get_track_artists(item):
 
 # GETTING TOP ARTISTS DATA
 
-def get_top_artists(term):
-    data = []
-    topartists = spotify.current_user_top_artists(time_range=term,limit=50)
-    for item in topartists.items:
-        data.append(get_artist_data(item))
-    return data
+def get_top_artists(term, token):
+    tempspotify = tk.Spotify()
+    with tempspotify.token_as(token):
+        data = []
+        topartists = tempspotify.current_user_top_artists(time_range=term,limit=50)
+        for item in topartists.items:
+            data.append(get_artist_data(item))
+        return data
 
 def get_artist_data(artist):
     if not len(artist.images):
-        pfp = "https://vignette.wikia.nocookie.net/caramella-girls/images/9/99/Blankpfp.png/revision/latest?cb=20190122015011"
+        pfp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     else:
         pfp = artist.images[0].url
     data = {
@@ -133,45 +137,44 @@ def error():
 
 @app.route('/', methods=['GET'])
 def main():
-    # try:
-    print(session)
-    user = session.get('user', None)
-    token = users.get(user, None)
-    page = ''
-
-    if user is None or token is None:
-        session.pop('user', None)
-        return render_template('index.html')
-
-    if token.is_expiring:
-        token = cred.refresh(token)
-        users[user] = token
-
-    with spotify.token_as(users[user]):
-        print("LOGIN: " + spotify.current_user().display_name)
+    try:
         print(session)
-        pprint(users.keys())
+        user = session.get('user', None)
+        token = users.get(user, None)
+        page = ''
 
-        userdata = get_user_data()
-        playbackdata = get_playback_data(users[user])
+        if user is None or token is None:
+            session.pop('user', None)
+            return render_template('index.html')
 
-        monthtracks = get_top_tracks('short_term')
-        yeartracks = get_top_tracks('medium_term')
-        alltimetracks = get_top_tracks('long_term')
+        if token.is_expiring:
+            token = cred.refresh(token)
+            users[user] = token
 
-        monthartists = get_top_artists('short_term')
-        yearartists = get_top_artists('medium_term')
-        alltimeartists = get_top_artists('long_term')
+        with spotify.token_as(users[user]):
+            print("LOGIN: " + spotify.current_user().display_name)
+            print(session)
+            pprint(users.keys())
+
+            userdata = get_user_data()
+            playbackdata = get_playback_data(users[user])
+
+            monthtracks = get_top_tracks('short_term', users[user])
+            yeartracks = get_top_tracks('medium_term', users[user])
+            alltimetracks = get_top_tracks('long_term', users[user])
+
+            monthartists = get_top_artists('short_term', users[user])
+            yearartists = get_top_artists('medium_term', users[user])
+            alltimeartists = get_top_artists('long_term', users[user])
 
 
-        return render_template('homepage.html', user=userdata,
-                    playback=playbackdata, month_tracks=monthtracks,
-                    year_tracks=yeartracks, alltime_tracks=alltimetracks,
-                    month_artists=monthartists, year_artists=yearartists,
-                    alltime_artists=alltimeartists, sessionid=session['user'])
-    return page
-    # except:
-    #     return render_template('error.html')
+            return render_template('homepage.html', user=userdata,
+                        playback=playbackdata, month_tracks=monthtracks,
+                        year_tracks=yeartracks, alltime_tracks=alltimetracks,
+                        month_artists=monthartists, year_artists=yearartists,
+                        alltime_artists=alltimeartists, sessionid=session['user'])
+    except:
+        return render_template('error.html')
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -220,7 +223,7 @@ def updatePlaybackData(sessionid):
                     data = get_playback_data(token)
                     return make_response(jsonify(data), 200)
             except:
-                print("ERROR REACHED: " + spotify.current_user().display_name)
+                print("ERROR REACHED: " + sessionid)
                 print(session)
                 return(jsonify({}), 400)
     except:
